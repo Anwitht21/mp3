dc346bf85033169cb87378522e8e0fb3b49d5cec
Anwitht21 <telluri.anwith@gmail.com>
2025-11-03 20:57:29 -0600

    fixed connection option

--- config/db.js
+++ config/db.js
@@ -1,7 +1,12 @@
 var mongoose = require('mongoose');
 
 module.exports = function() {
-    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+    var options = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    };
+    
+    mongoose.connect(process.env.MONGODB_URI, options)
         .then(() => {
             console.log('MongoDB connected successfully');
         })
